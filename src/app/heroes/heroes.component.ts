65764778c8dbf9e58e2dc547fabf49277d807d14
smalutwela <sameera.alutwela@yahoo.com>
2022-04-27 10:44:03 +0530

    integrating a mock service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-// import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,13 +15,31 @@ export class HeroesComponent implements OnInit {
   //   name: 'Windstorm'
   // }
 
-  hehe = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[] = []; //test
 
-  constructor() { }
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { } //test
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    // console.log(this.selectedHero);
+  }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
 
-console.log(HEROES[1]);
+// console.log(heroes);
